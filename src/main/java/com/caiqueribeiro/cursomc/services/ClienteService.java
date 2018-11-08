7ed379f8af699f62be7f834686858143736e5d5d
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2018-11-08 20:13:16 -0200

    Endpoint /cliente/{id} disponível

---
+++ src/main/java/com/caiqueribeiro/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.caiqueribeiro.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caiqueribeiro.cursomc.domain.Cliente;
+import com.caiqueribeiro.cursomc.repositories.ClienteRepository;
+import com.caiqueribeiro.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Erro: Não foi encontrado id " + id + ", Tipo: " + Cliente.class.getName()));		
+		
+	}
+
+}
