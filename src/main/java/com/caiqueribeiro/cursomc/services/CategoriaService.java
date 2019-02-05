e93ee1513859f153938169e0f5ef1832f4117c30
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2019-02-05 21:52:45 -0200

    Validacao sintatica com Bean Validation

--- src/main/java/com/caiqueribeiro/cursomc/services/CategoriaService.java
+++ src/main/java/com/caiqueribeiro/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.caiqueribeiro.cursomc.domain.Categoria;
+import com.caiqueribeiro.cursomc.dto.CategoriaDTO;
 import com.caiqueribeiro.cursomc.repositories.CategoriaRepository;
 import com.caiqueribeiro.cursomc.services.exceptions.DataIntegrityException;
 import com.caiqueribeiro.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
